6406d7065568c57a09388c186f23e73ae207ea4b
Andrés G. Aragoneses <knocte@gmail.com>
2015-07-01 01:31:29 +0200

    [banshee] switch to stable-2.6 branch
    Get rid of the way to build banshee master,
    since we're in the stable-2.6 branch of
    bockbuild.

--- packages/banshee.py
+++ packages/banshee.py
@@ -1,6 +1,6 @@
-class BansheeBasePackage (Package):
-	def __init__ (self, version):
-		Package.__init__ (self, 'banshee', version)
+class BansheePackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'banshee', '2.6.3', git_branch = 'stable-2.6')
 
 		self.sources = [
 			'git://git.gnome.org/banshee',
@@ -21,26 +21,4 @@ class BansheeBasePackage (Package):
 		self.sh ('patch -p1 < "%{sources[1]}"')
 		self.sh ('patch -p0 < "%{sources[2]}"')
 
-class BansheePackage (BansheeBasePackage):
-	def __init__ (self, force_latest_git_master = False):
-		global BansheeBasePackage;
-
-		if (Package.profile.cmd_options.release_build and not force_latest_git_master):
-			BansheeBasePackage.__init__ (self, 'banshee-2.6')
-			# release build, specify stable git branch
-			self.git_branch = 'stable-2.6'
-
-			self.sources.extend([
-			])
-
-		else:
-			BansheeBasePackage.__init__ (self, 'master')
-			# non-release build, use latest git master
-			self.git_branch = 'master'
-
-			self.sources.extend([
-			])
-
-# you can set force_latest_git_mater to True when you want to build latest
-# source from git for testing purposes but use packages from the release list
-BansheePackage (force_latest_git_master = False)
+BansheePackage ()
