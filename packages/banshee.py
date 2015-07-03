9cc9d346a7996d4e8432a05fb5d90ae87aaa0265
Andrés G. Aragoneses <knocte@gmail.com>
2015-07-03 02:42:25 +0200

    [banshee] remove the last patch left
    This patch was already not being applied (because
    it had already been committed upstream long ago[1]),
    but somebody forgot to remove it from the patches
    directory.
    
    [1] https://git.gnome.org/browse/banshee/commit/?h=stable-2.6&id=d71b1031124424fcfa226536c57a4575a635ad7c

--- packages/banshee.py
+++ packages/banshee.py
@@ -8,10 +8,4 @@ class BansheePackage (Package):
 
 		self.configure = [ 'NOCONFIGURE=1 ./autogen.sh && ./profile-configure %{profile.name} --prefix=%{prefix}' ]
 
-	def prep (self):
-		Package.prep (self)
-		#for p in range (1, len (self.sources)):
-		#	self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
-		#self.sh ('patch -p1 < "%{sources[1]}"')
-
 BansheePackage ()
