3e6eb41211a0ffb7e61adcfcaf92e17f8daa2d70
Andrés G. Aragoneses <knocte@gmail.com>
2015-07-03 02:26:32 +0200

    [banshee] remove obsolete patch
    Another patch that has already been applied
    to banshee's official repo, here:
    
    https://git.gnome.org/browse/banshee/commit/?h=stable-2.6&id=029f7dacd1bb3e8643aa82f686be8c0543b44081

--- packages/banshee.py
+++ packages/banshee.py
@@ -4,9 +4,6 @@ class BansheePackage (Package):
 
 		self.sources = [
 			'git://git.gnome.org/banshee',
-
-			# fix configure complaining about missing HAVE_DBUS_GLIB conditional
-			'patches/banshee-fix-dbus-conditional.patch',
 		]
 
 		self.configure = [ 'NOCONFIGURE=1 ./autogen.sh && ./profile-configure %{profile.name} --prefix=%{prefix}' ]
@@ -15,7 +12,6 @@ class BansheePackage (Package):
 		Package.prep (self)
 		#for p in range (1, len (self.sources)):
 		#	self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
-		self.sh ('patch -p1 < "%{sources[1]}"')
-		self.sh ('patch -p0 < "%{sources[2]}"')
+		#self.sh ('patch -p1 < "%{sources[1]}"')
 
 BansheePackage ()
